03996142a3af5f0892bd248b8e12cb571a7c8206
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-04-21 01:59:13 +0600

    root layout add

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/providers/themeProvider";
+import Navbar from "@/components/layouts/Navbar/Navbar";
+import FooterSection from "@/components/shared/FooterSection/FooterSection";
+import QueryProviders from "@/providers/QueryProvider";
+import { Toaster } from "@/components/ui/sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Eco Pulse",
+  description:
+    "Eco Pulse is a web application that helps users track their carbon footprint and make sustainable choices.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <QueryProviders>
+          <Toaster position="top-right" />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            {children}
+            <FooterSection />
+          </ThemeProvider>
+        </QueryProviders>
+      </body>
+    </html>
+  );
+}
